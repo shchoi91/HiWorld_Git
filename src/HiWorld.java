f8838eea1f5a744001f7f33e29f5df0b6d082e27
shchoi <shchoicoder@gmail.com>
2021-04-08 00:53:51 +0900

    반복

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -4,6 +4,10 @@ public class HiWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hi World!");
+		System.out.println("Hi World!");
+		System.out.println("Hi World!");
+		System.out.println("Hi World!");
+		System.out.println("Hi World!");
 		
 	}
 
